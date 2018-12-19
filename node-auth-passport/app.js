bc8988b19e100bc1f1f0df72e1df20c39f290c22
AaronFlower <haojunzhan@gmail.com>
2018-12-19 16:13:16 +0800

    add node auth

---
+++ node-auth-passport/app.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const errorHandler = require('errorhandler')
+
+// Configuration mongoose's promise to global promise
+mongoose.promise = global.Promise
+
+
+// Configure isProduction variable
+const isProduction = process.env.NODE_ENV === 'production'
+
+// Initiate our app
+const app = express()
+
+// Configure our app
+app.use(cors())
+app.use(require('morgan')('dev'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(session({
+    secret: 'passport-tutorial',
+    cookie: {maxAge: 60000},
+    resave: false,
+    saveUninitialized: false
+}))
+
+if (!isProduction) {
+    app.use(errorHandler())
+}
+
+// Configure Mongoose
+mongoose.connect('mongodb://localhost/passport-tutorial', { useNewUrlParser: true })
+mongoose.set('debug', true)
+
+// Models & routes
+require('./models/user.js')
+require('./config/passport.js')
+app.use(require('./routes'))
+
+if (!isProduction) {
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500)
+        res.json({
+            errors: {
+                message: err.message,
+                error: err,
+            },
+        })
+    })
+}
+
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500)
+        res.json({
+            errors: {
+                message: err.message,
+                error: err,
+            },
+        })
+    })
+
+app.listen(8000, () => console.log('Server running on http://localhost:8000/'))
---
+++ node-auth-passport/readme.md
@@ -0,0 +1,22 @@
+Guided by [Handle Authentication](https://medium.freecodecamp.org/learn-how-to-handle-authentication-with-node-using-passport-js-4a56ed18e81e)
+## Requirements
+- node
+- npm
+- MongoDB
+
+## Run
+```
+npm i
+node app.js
+```
+
+
+## MongoDB
+```bash
+mongo
+show dbs;
+use passport-tutorial
+show collections;
+db.users.find()
+```
+
